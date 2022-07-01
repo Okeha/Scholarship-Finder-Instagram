f75a33b1dcc3937fe8c821f39a2b06abdc8caf6d
Okeha <okehanthony1234@gmail.com>
2022-07-01 22:45:28 +0100

    Made temporary changes to the front end, and added some new pages. Might delete later lets just see.

--- views/javascript/script.js
+++ views/javascript/script.js
@@ -16,3 +16,29 @@ function closeMenu() {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }
+
+$(document).ready(function () {
+  $("#search").focus(function () {
+    $(".search-box").addClass("border-searching");
+    $(".search-icon").addClass("si-rotate");
+  });
+  $("#search").blur(function () {
+    $(".search-box").removeClass("border-searching");
+    $(".search-icon").removeClass("si-rotate");
+  });
+  $("#search").keyup(function () {
+    if ($(this).val().length > 0) {
+      $(".go-icon").addClass("go-in");
+    } else {
+      $(".go-icon").removeClass("go-in");
+    }
+  });
+  $(".go-icon").click(function () {
+    $(".search-form").submit();
+  });
+});
+
+// const button = document.getElementById("btn1");
+// button.addEventListener("click", () => {
+//   location.href = "../html/Scholarship-finder.html";
+// });
