9f19c7e5f4a15259a8ca1891909f9c4aa8c24e35
Okeha <okehanthony1234@gmail.com>
2022-07-10 21:13:24 +0100

    First Commit

--- config/conn.js
+++ config/conn.js
@@ -1,9 +1,18 @@
-const conn = {
+const con = {
   connectionLimit: 10000,
   host: "localhost",
   user: "root",
   password: "",
   database: "ig_scholarship_finder",
+  port: "3306",
+};
+
+const conn = {
+  connectionLimit: 10000,
+  host: "demo.phpmyadmin.net",
+  user: "root",
+  password: "",
+  database: "ig",
   port: "3306",
 };
 
